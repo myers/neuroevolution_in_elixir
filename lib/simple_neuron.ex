e8fab35166acd8d28113a3ca9be8db3cef63f902
Myers Carpenter <myers@maski.org>
2014-12-03 12:32:59 -0500

    Wrap comments

--- lib/simple_neuron.ex
+++ lib/simple_neuron.ex
@@ -1,13 +1,16 @@
 # From 6.1 Simulating A Neuron
 defmodule SimpleNeuron do
 
-  # The create function spawns a single neuron, where the weights and the bias are generated randomly to be between -0.5 and 0.5
+  # The create function spawns a single neuron, where the weights and the bias
+  # are generated randomly to be between -0.5 and 0.5
   def create do
     weights = [:random.uniform() - 0.5, :random.uniform() - 0.5, :random.uniform() - 0.5]
     Process.register(spawn(__MODULE__, :loop, [weights]), :neuron)
   end
 
-  # The spawned neuron process accepts and input vector, prints it and the weight vector to the screen, calculates the output, and then sends the output to the contacting process. The outpu is also a vector of length one.
+  # The spawned neuron process accepts and input vector, prints it and the
+  # weight vector to the screen, calculates the output, and then sends the
+  # output to the contacting process. The outpu is also a vector of length one.
   def loop(weights) do
     receive do
       {from, input} ->
@@ -16,10 +19,14 @@ defmodule SimpleNeuron do
         output = [:math.tanh(dot_product)]
         from |> send({:result, output})
         loop(weights)
-    end    
+    end
   end
 
-  # The dot product function that we use works on the assumption that the bias is incorporated into the weight list as the last value in that list.  After calculation the dot product, the input list will empty out while the weight list will still have the single biase value remaining, which we then add to the accumulator.
+  # The dot product function that we use works on the assumption that the bias
+  # is incorporated into the weight list as the last value in that list.  After
+  # calculation the dot product, the input list will empty out while the weight
+  # list will still have the single biase value remaining, which we then add to
+  # the accumulator.
   def dot([i | input], [w | weights], acc) do
     dot(input, weights, i*w+acc)
   end
@@ -27,7 +34,9 @@ defmodule SimpleNeuron do
     acc + bias
   end
 
-  # We use the sense function to contact the neuron and send it an input vector.  The sense function ensures that the signal we are sending is a vector of length 2.
+  # We use the sense function to contact the neuron and send it an input vector.
+  # The sense function ensures that the signal we are sending is a vector of
+  # length 2.
   def sense(signal) do
     case is_list(signal) and (length(signal) == 2) do
       true -> 
--- lib/simplest_nn.ex
+++ lib/simplest_nn.ex
@@ -1,6 +1,9 @@
 # From 6.2 A One Neuron Neural Network
 defmodule SimplestNN do
-  # The create function first generates 3 weights, with the 3rd weigth being the bias. The neuron is spawed first, and is then sent the pids of the sensor and actuator that it's connected with.  Then the cortex element is registered and provided with the pids of all the elements in the NN system.
+  # The create function first generates 3 weights, with the 3rd weigth being the
+  # bias. The neuron is spawed first, and is then sent the pids of the sensor
+  # and actuator that it's connected with.  Then the cortex element is
+  # registered and provided with the pids of all the elements in the NN system.
   def create do
     weights = [:random.uniform() - 0.5, :random.uniform() - 0.5, :random.uniform() - 0.5]
     n_pid = spawn __MODULE__, :neuron, [weights, nil, nil]
@@ -10,15 +13,19 @@ defmodule SimplestNN do
     Process.register(spawn(__MODULE__, :cortex, [s_pid, n_pid, a_pid]), :cortex)
   end
 
-  # After the neuron finishes setting its s_pid and a_pid to that of the senor and actuator respectively, it starts waiting for the incoming signals.  The neuron expects a vector of length 2 as input, and as soon as the input arrives, the neuron processes the signal and passes the output vector to the outgoing a_pid
-  def neuron(weights, s_pid, a_pid) do
+  # After the neuron finishes setting its s_pid and a_pid to that of the senor
+  # and actuator respectively, it starts waiting for the incoming signals.  The
+  # neuron expects a vector of length 2 as input, and as soon as the input
+  # arrives, the neuron processes the signal and passes the output vector to the
+  # outgoing a_pid
+  def neuron(weights, _s_pid, a_pid) do
     receive do
-      {s_pid, :forward, input} ->
+      {_s_pid, :forward, input} ->
         IO.puts "**** Thinking ****\n Input: #{inspect input}\n with weights: #{inspect weights}"
         dot_product = dot(input, weights, 0)
         output = [:math.tanh(dot_product)]
         a_pid |> send({self, :forward, output})
-        neuron(weights, s_pid, a_pid)
+        neuron(weights, _s_pid, a_pid)
       {:init, new_s_pid, new_a_pid} ->
         neuron(weights, new_s_pid, new_a_pid)
       :terminate ->
@@ -26,7 +33,11 @@ defmodule SimplestNN do
     end
   end
 
-  # The dot function take a dot product of two vectors, it can operate on a weight vector with and without a bias.  When there is no bias in the weight list, both the input vector and the weight vector are of the same length.  When bias is present, then when the input list empties out, the weights list still has 1 value remaining, its bias.
+  # The dot function take a dot product of two vectors, it can operate on a
+  # weight vector with and without a bias.  When there is no bias in the weight
+  # list, both the input vector and the weight vector are of the same length.
+  # When bias is present, then when the input list empties out, the weights list
+  # still has 1 value remaining, its bias.
   def dot([i | input], [w | weights], acc) do
     dot(input, weights, i*w+acc)
   end
@@ -34,7 +45,12 @@ defmodule SimplestNN do
     acc + bias
   end
 
-  # The sensor function waits to be triggered by the cortex element, and then produces a random vector of length 2, which it passes to the connected neuron.  In a proper system the sensory signal would not be a random vector but instead would be produced by a function associated with the sensor, a function that for example reads and vector-encodes a signal coming from a GPS attached to a robot
+  # The sensor function waits to be triggered by the cortex element, and then
+  # produces a random vector of length 2, which it passes to the connected
+  # neuron.  In a proper system the sensory signal would not be a random vector
+  # but instead would be produced by a function associated with the sensor, a
+  # function that for example reads and vector-encodes a signal coming from a
+  # GPS attached to a robot
   def sensor(n_pid) do
     receive do
       :sync ->
@@ -46,12 +62,14 @@ defmodule SimplestNN do
     end
   end
 
-  # The actuator function waits for a control signal coming from a neuron.  As soon as the signal arrives, the actuator executes its function pts/1, which prints the value to the screen.
-  def actuator(n_pid) do
+  # The actuator function waits for a control signal coming from a neuron.  As
+  # soon as the signal arrives, the actuator executes its function pts/1, which
+  # prints the value to the screen.
+  def actuator(_n_pid) do
     receive do
-      {n_pid, :forward, control_signal} ->
+      {_n_pid, :forward, control_signal} ->
         pts(control_signal)
-        actuator(n_pid)
+        actuator(_n_pid)
       :terminate ->
         :ok
     end
@@ -61,7 +79,9 @@ defmodule SimplestNN do
     IO.puts "**** Acting ****\n Using: #{inspect control_signal} to act on env"
   end
 
-  # The cortex function triggers the sensor to action when commanded by the user.  This process also has all the pids of the elements in the NN system, so that it can terminate the whole system when requested.
+  # The cortex function triggers the sensor to action when commanded by the
+  # user.  This process also has all the pids of the elements in the NN system,
+  # so that it can terminate the whole system when requested.
   def cortex(s_pid, n_pid, a_pid) do
     receive do
       :sense_think_act ->
